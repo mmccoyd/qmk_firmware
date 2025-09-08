cb1b58ca9b3659b232dd718caca4330572bfb26e
mmccoyd <mmccoyd@cs.berkley.edu>
2025-09-08 15:16:51 -0700

    feat(keymap): thumbs down a row for bigger fonts

--- keyboards/hillside/d50/keymaps/hardware_test/keymap.c
+++ keyboards/hillside/d50/keymaps/hardware_test/keymap.c
@@ -19,9 +19,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      //
      // column pins:
      // 0      1        2        3        4        5                                               5        4        3        2        1        0
-     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, // row pin 0
-     UG_TOGG, UG_VALU, UG_NEXT, KC_D,    KC_F,    EE_CLR,                                         EE_CLR,  KC_J,    KC_K,    UG_NEXT, UG_VALU, UG_TOGG, // row pin 1
-     KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_1,   KC_2,        KC_2,    KC_1,    KC_5,    KC_4,    KC_3,    KC_2,    KC_1,    KC_0,    // row pin 2
-                       KC_X,    KC_C,             KC_5,    KC_4,   KC_3,        KC_3,    KC_4,    KC_5,             KC_COMM, KC_DOT                     // row pin 3
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, // row pin 0
+     UG_TOGG, UG_VALU, UG_NEXT, KC_D,    KC_F,    EE_CLR,             EE_CLR,  KC_J,    KC_K,    UG_NEXT, UG_VALU, UG_TOGG, // row pin 1
+     KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,               KC_5,    KC_4,    KC_3,    KC_2,    KC_1,    KC_0,    // row pin 2
+                       KC_X,    KC_C,        KC_1,   KC_2,        KC_2,    KC_1,        KC_COMM, KC_DOT,                    // row pin 3
+                                    KC_5,    KC_4,   KC_3,        KC_3,    KC_4,    KC_5
     ),                                         // Row pin 4 are the thumbs, which return their column pin
 };
